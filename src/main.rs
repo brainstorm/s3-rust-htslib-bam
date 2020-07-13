0b9fb022e7e347fd819990b901265917e2aa23cb
Roman Valls Guimera <brainstorm@nopcode.org>
2020-07-13 16:28:16 +1000

    Add in logic so that the response is a proper JSON, otherwise lambda_http does 400 as expected

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,44 @@
 use url::Url;
+
+use serde_derive::{Serialize, Deserialize};
+use lambda_http::{
+    handler,
+    lambda::{self, Context},
+    IntoResponse, Request, RequestExt, Response,
+};
 use crate::reader::BamReader;
 use rust_htslib::htslib;
 
 pub mod reader;
 pub mod errors;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // Get some lowlevel libcurl action on hfile_curl/s3 from htslib
+    // WARNING: Disable for production use as it prints out secret tokens on CloudWatch!
+    // hts_set_log_level(10);
+
+    // And run the lambda
+    lambda::run(handler(bam_header)).await?;
+    Ok(())
+}
+
+async fn bam_header(event: Request, _: Context) -> Result<impl IntoResponse, Error> {
     const BUCKET: &str = "umccr-research-dev";
     const KEY: &str = "htsget/htsnexus_test_NA12878.bam";
+
     // const BUCKET: &str = "gatk-test-data";
     // const KEY: &str = "wgs_bam/NA12878_24RG_hg38/NA12878_24RG_small.hg38.bam";
+    let bam_head = bam_header_s3(BUCKET, KEY);
 
-    // Get some lowlevel libcurl action on hfile_curl/s3 from htslib
-    // Disable for production use as it prints out secret tokens on CloudWatch!
-    hts_set_log_level(10);
-    let res = bam_header_s3(BUCKET, KEY);
-
-    // Show the BAM header targets on stdout
-    dbg!("BAM header targets are: {}", res);
-    Ok(())
+    Ok(match event.body() {
+        _ => Response::builder()
+            .status(200)
+            .body(serde_json::to_string(&bam_head))
+            .expect("failed to render response"),
+    })
 }
 
 pub fn bam_header_s3(bucket: &str, key: &str) -> Vec<String> {
@@ -30,5 +50,25 @@ pub fn bam_header_s3(bucket: &str, key: &str) -> Vec<String> {
 pub fn hts_set_log_level(level: u32) {
     unsafe {
         htslib::hts_set_log_level(level);
+    }
+}
+
+// Lambda-specific response structs
+#[derive(Serialize, Deserialize, Clone)]
+struct Body {
+    #[serde(rename = "bam_header")]
+    bam_header: Vec<String>,
+}
+
+#[derive(Serialize, Clone)]
+struct CustomOutput {
+    body: String,
+}
+
+impl CustomOutput {
+    fn new(body: String) -> Self {
+        CustomOutput {
+            body,
+        }
     }
 }
