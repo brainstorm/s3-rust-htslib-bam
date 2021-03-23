bbcdee8b15c9afbe724ceb03ed3acc69683e2fd9
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2021-03-23 03:48:01 +0000

    Separate bam_header and stream_s3_object, need to match types now...

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,64 @@
-use lambda_runtime::{handler_fn, Context};
-use serde_json::Value;
+use lambda_runtime::{ handler_fn, Context };
+use serde_json::{ json, Value };
 
 use s3::bucket::Bucket;
 use s3::creds::Credentials;
 use s3::region::Region;
 use s3::S3Error;
 
+use noodles_bam as bam;
+use noodles_sam as sam;
+
 struct Storage {
-    name: String,
     region: Region,
     credentials: Credentials,
     bucket: String,
-    location_supported: bool,
 }
+
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 #[tokio::main]
-async fn main() -> Result<(), S3Error> {
-    lambda_runtime::run(handler_fn(bam_header)).await;
-    let mut output = std::io::stdout();
-    let aws = Storage {
-        name: "aws".into(),
-        region: "ap-southeast-2".parse()?,
-        // credentials: Credentials::from_profile(Some("rust-s3"))?,
-        credentials: Credentials::from_instance_metadata()?,
-        bucket: "umccr-primary-data-dev".to_string(),
-        location_supported: true,
-    };
-    let bucket = Bucket::new(&aws.bucket, aws.region, aws.credentials)?;
-    bucket.get_object_stream("sample-file.bam", &mut output);
-    // let (data, code) = bucket.get_object_blocking("test_file")?;
-    // let string = str::from_utf8(&data)?;
+async fn main() -> Result<(), Error> {
+    lambda_runtime::run(handler_fn(cloud_lambda_bioinfo)).await;
     Ok(())
 }
 
-async fn bam_header(_: Value, _: Context) -> Result<(), S3Error> {
-    Ok(())
+async fn cloud_lambda_bioinfo(_: Value, _: Context) -> Result<Value, Error> {
+    let s3_object = stream_s3_object().await?;
+    let output = bam_header(s3_object).await?;
+    Ok(json!({ "message": output }))
+}
+
+async fn stream_s3_object() -> Result<u16, S3Error> {
+    let mut output = std::io::stdout();
+    let aws = Storage {
+        region: "ap-southeast-2".parse()?,
+        credentials: Credentials::from_instance_metadata()?,
+        bucket: "umccr-primary-data-dev".to_string(),
+    };
+
+    let bucket = Bucket::new(&aws.bucket, aws.region, aws.credentials)?;
+    return bucket.get_object_stream("sample-file.bam", &mut output).await;
+}
+
+async fn bam_header(bam_bytes: u16) -> Result<Value, Error> {
+    let mut reader = bam::Reader::new(u16::from(bam_bytes));
+    let header = reader.read_header()?;
+
+    let res;
+
+    if header.is_empty() {
+        let reference_sequences = reader.read_reference_sequences()?;
+        let mut builder = sam::Header::builder();
+
+        for reference_sequence in reference_sequences {
+            builder = builder.add_reference_sequence(reference_sequence);
+        }
+
+        res = builder.build();
+    } else {
+        res = header;
+    }
+
+    Ok(json!({ "header": res.to_string() }))
 }
